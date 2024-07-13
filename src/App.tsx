e503ed3a29b5ae7571c37e37a925abd4cf558785
Erik Mejia <erik@erikmejia.com>
2024-07-13 10:02:40 -0400

    feat: add initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [dynamicText, setDynamicText] = useState('');
+  const [age, setAge] = useState('');
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="root">
+      <form>
+        <div className="field-group">
+          <label>Name:</label>
+          <input className='input' type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div className="field-group">
+          <label>Email:</label>
+          <input className='input' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+        <div className="field-group">
+          <label>Dynamic Text:</label>
+          <input className='input' type="text" value={dynamicText} onChange={(e) => setDynamicText(e.target.value)} />
+        </div>
+        <div className="field-group">
+          <label>Age:</label>
+          <input className='input' type="number" value={age} onChange={(e) => setAge(e.target.value)} />
+        </div>
+        <button className='submit-button' type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
